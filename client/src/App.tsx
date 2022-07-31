ebd277b974717bc65206d0f914afa3fe4a2ad8d9
hajajon <amitha@wix.com>
2022-07-31 16:14:41 +0300

    Add routing with 'react-router-dom'. and some css attributes.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Map } from './components/Map'
-import { ResponsiveAppBar } from './components/Header'
+import { Home } from './pages/Home'
+import ResponsiveAppBar from './components/Header'
+// @ts-ignore
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {FlightsPrediction} from "./pages/FlightsPrediction";
 
 function App() {
   return (
-    <div className="App">
-        <ResponsiveAppBar/>
-        <Map/>
-    </div>
+      <Router>
+        <div className="App">
+            <ResponsiveAppBar/>
+            <Routes>
+                <Route path="/" element={<Home/>}>
+                </Route>
+                <Route path="predict-flights" element={<FlightsPrediction/>}>
+                </Route>
+            </Routes>
+        </div>
+      </Router>
   );
 }
 
 export default App;
+
+//69766e1b1f0ca7f8e7ff02124be9a796
