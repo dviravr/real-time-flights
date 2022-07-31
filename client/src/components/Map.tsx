2c62b32bb241d41293df3fcae5c5b67021865302
hajajon <amitha@wix.com>
2022-07-31 16:15:49 +0300

    Add needed components for the app. Weather component is broken and currently unused.

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,17 +1,11 @@
 export const Map = () => {
     return (
-        <div style={{alignItems: 'center'}}>
-            <div style={{whiteSpace: 'nowrap', textAlign: 'center', width: '2000px', height: '2000', padding: '100px'}}>
+        <div className="BingMap">
+            <div>
                 <iframe width="1000" height="800"
-                        src="https://www.bing.com/maps/embed?h=400&w=500&cp=32.06692659097088~34.78262995523687&lvl=10.950990165584006&typ=d&sty=r&src=SHELL&FORM=MBEDV8"
+                        src="https://www.bing.com/maps/embed?h=800&w=1000&cp=32.06692659097088~34.78262995523687&lvl=10.950990165584006&typ=d&sty=r&src=SHELL&FORM=MBEDV8"
                         scrolling="no" style={{display: 'block', borderStyle: 'none' ,margin:'0 auto', width:'1000', height: '500'}}>
                 </iframe>
-                <a id="largeMapLink" target="_blank"
-                   href="https://www.bing.com/maps?cp=32.06692659097088~34.78262995523687&amp;sty=r&amp;lvl=10.950990165584006&amp;FORM=MBEDLD">View
-                    Larger Map</a> &nbsp; | &nbsp;
-                <a id="dirMapLink" target="_blank"
-                   href="https://www.bing.com/maps/directions?cp=32.06692659097088~34.78262995523687&amp;sty=r&amp;lvl=10.950990165584006&amp;rtp=~pos.32.06692659097088_34.78262995523687____&amp;FORM=MBEDLD">Get
-                    Directions</a>
             </div>
         </div>
     )
