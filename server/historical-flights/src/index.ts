5f9dea6c075bd656c797558904e1ef7064b6a5a4
Dvir <dviravr@gmail.com>
2022-08-26 13:10:46 +0300

    bigml prediction

--- server/historical-flights/src/index.ts
+++ server/historical-flights/src/index.ts
@@ -3,8 +3,7 @@ import { json } from 'body-parser';
 import { exampleRouter } from './core/routes/example';
 import { config, ConsumerService } from 'real-time-flight-lib';
 import { connectToDB } from './services/db.service';
-import { saveHistoricalFlight } from './services/histoical-flight.service';
-import { createDatafile } from './services/bigML.service';
+import { bigML } from './services/bigML.service';
 
 const app = express();
 const port = 3000;
@@ -17,38 +16,11 @@ app.use(exampleRouter);
 app.listen(port, async () => {
   console.log(`Express is listening at http://localhost:${ port }`);
   await connectToDB();
-  // createDatafile();
-
-  await historicalFlightsConsumer.consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      console.log(JSON.parse(message.value.toString()));
-      saveHistoricalFlight(JSON.parse(message.value.toString()));
-    },
-  });
+  await bigML();
+  // await historicalFlightsConsumer.consumer.run({
+  //   eachMessage: async ({ topic, partition, message }) => {
+  //     console.log(JSON.parse(message.value.toString()));
+  //     saveHistoricalFlight(JSON.parse(message.value.toString()));
+  //   },
+  // });
 });
-
-
-// const bigml = require('bigml');
-// const source = new bigml.Source();
-// const connection = new bigml.BigML('dviravr', '50193977c80c720a87bb5540867fc84c248e26bc');
-//
-// source.create('./iris.csv', (error, sourceInfo) => {
-//   if (!error && sourceInfo) {
-//     const dataset = new bigml.Dataset();
-//     dataset.create(sourceInfo, (error, datasetInfo) => {
-//       if (!error && datasetInfo) {
-//         const model = new bigml.Model();
-//         model.create(datasetInfo, (error, modelInfo) => {
-//           if (!error && modelInfo) {
-//             const prediction = new bigml.Prediction();
-//             prediction.create(modelInfo, { 'petal length': 1 });
-//           }
-//         });
-//       }
-//     });
-//   }
-// });
-
-
-
-
--- server/historical-flights/src/services/bigML.service.ts
+++ server/historical-flights/src/services/bigML.service.ts
@@ -1,25 +1,11 @@
 import { getAllFlights } from './db.service';
 import { Flight } from 'real-time-flight-lib';
+import { isNil } from 'lodash';
+import { HebrewCalendar } from '@hebcal/core';
+import { Dataset, Model, Prediction, Source, BigML } from 'bigml';
 
-const bigml = require('bigml');
-const source = new bigml.Source();
-
-source.create('./iris.csv', (error, sourceInfo) => {
-  if (!error && sourceInfo) {
-    const dataset = new bigml.Dataset();
-    dataset.create(sourceInfo, (error, datasetInfo) => {
-      if (!error && datasetInfo) {
-        const model = new bigml.Model();
-        model.create(datasetInfo, (error, modelInfo) => {
-          if (!error && modelInfo) {
-            const prediction = new bigml.Prediction();
-            prediction.create(modelInfo, { 'petal length': 1 });
-          }
-        });
-      }
-    });
-  }
-});
+const connection = new BigML('DVIRAVR', '50193977c80c720a87bb5540867fc84c248e26bc');
+const source = new Source(connection);
 
 enum PeriodOfTheYear {
   REGULAR,
@@ -59,20 +45,28 @@ const getFlightDelayRate = (scheduledTime: number, actualTime: number): DelayRat
   return DelayRate.HAVEY_DELAY;
 };
 
+const isHoliday = (day: Date): boolean => {
+  return isNil(HebrewCalendar.getHolidaysOnDate(day, true));
+};
+
 const getPeriodOfTheYear = (day: Date): PeriodOfTheYear => {
   if (day.getMonth() === 6 || day.getMonth() === 7) {
     return PeriodOfTheYear.SUMMER;
+  } if (isHoliday(day)) {
+    return PeriodOfTheYear.HOLIDAYS;
   }
   return PeriodOfTheYear.REGULAR;
 };
 
-export const createDatafile = async () => {
+const createDatafile = async () => {
   const allFlights: Flight[] = await getAllFlights();
-  const mappedFlights = allFlights.map((flight) => {
+  return allFlights.map((flight) => {
     return {
       airline: flight.airline,
       origin: flight.origin.country,
       destination: flight.destination.country,
+      originWeather: flight.origin.weather,
+      destinationWeather: flight.destination.weather,
       distanceFlightType: getFlightTypeByDistance(flight.distance),
       dayInWeek: new Date(flight.actualTime.departureTime * 1000).getDay(),
       monthInYear: new Date(flight.actualTime.departureTime * 1000).getMonth(),
@@ -83,12 +77,45 @@ export const createDatafile = async () => {
   });
 };
 
-export const createDataSetBigML = () => {
+export const bigML = async () => {
+  const data = await createDatafile();
+  const flight = {
+    airline: 'El Al',
+    origin: 'Netherlands',
+    destination: 'Israel',
+    originWeather: 'Fog',
+    destinationWeather: 'Sunny',
+    distanceFlightType: 1,
+    dayInWeek: 4,
+    monthInYear: 7,
+    periodOfYear: 2,
+  };
 
-};
-export const createModelBigML = () => {
+  const prediction = new Prediction(connection);
+  prediction.create('model/630893098f679a2d430009a2', flight, (error, predictionInfo) => {
+    if (!error && predictionInfo) {
+      console.log(predictionInfo);
+    } else {
+      console.log(error);
+    }
+  });
 
-};
-export const predictionBigML = () => {
-
+  // source.create(data, (error, sourceInfo) => {
+  //   if (!error && sourceInfo) {
+  //     const dataset = new Dataset();
+  //     dataset.create(sourceInfo, (error, datasetInfo) => {
+  //       if (!error && datasetInfo) {
+  //         const model = new Model();
+  //         model.create(datasetInfo, (error, modelInfo) => {
+  //           if (!error && modelInfo) {
+  //             const prediction = new Prediction();
+  //             prediction.create(modelInfo, { 'petal length': 1 });
+  //           }
+  //         });
+  //       }
+  //     });
+  //   } else {
+  //     console.log(error);
+  //   }
+  // });
 };
