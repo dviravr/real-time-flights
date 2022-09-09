4fd875485c337acc4ee09f00bce417041f2f436d
Dvir <dviravr@gmail.com>
2022-09-09 11:26:51 +0300

    fix api requests

--- server/historical-flights/src/core/routes/bigml-router.ts
+++ server/historical-flights/src/core/routes/bigml-router.ts
@@ -7,12 +7,20 @@ import moment from 'moment';
 const router = express.Router();
 
 router.post('/bigml/createArrivalsModelByDate', ((req, res) => {
-  const startDate = new Date(1661806800 * 1000);
-  const endDate = new Date(1661893200 * 1000);
+  if (!req?.body?.startDate || !req?.body?.endDate) res.status(400).send('missing startDate or endDate');
+  createModelByTypeAndDates(
+      FlightsTypes.ARRIVALS,
+      moment(req.body.startDate).startOf('day'),
+      moment(req.body.endDate).startOf('day'),
+      (body: string, status: number) => res.status(status).send(body));
+}));
+
+router.post('/bigml/createDeparturesModelByDate', ((req, res) => {
+  if (!req?.body?.startDate || !req?.body?.endDate) res.status(400).send('missing startDate or endDate');
   createModelByTypeAndDates(
       FlightsTypes.DEPARTURES,
-      moment(startDate).startOf('day'),
-      moment(endDate).startOf('day'),
+      moment(req.body.startDate).startOf('day'),
+      moment(req.body.endDate).startOf('day'),
       (body: string, status: number) => res.status(status).send(body));
 }));
 
@@ -25,7 +33,7 @@ router.post('/bigml/createDeparturesModel', ((req, res) => {
 }));
 
 router.post('/bigml/predictFlight', ((req, res) => {
-  const modelDates = req.body.startDate && req.body.endDate ? { startDate: req.body.startDate, endDate: req.body.endDate } : undefined;
+  const modelDates = req?.body?.startDate && req?.body?.endDate ? { startDate: req.body.startDate, endDate: req.body.endDate } : undefined;
   predictFlights(
       req.body.flights,
       (body: Array<{ id: string; prediction: string }> | string, status: number) => res.status(status).send(body),
